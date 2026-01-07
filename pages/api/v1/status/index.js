ed2b082ceeedc149e6c27c7cffb7b1634af3715c
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-07 00:20:46 -0300

    add `database.js` and `.env` file + configurations

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function Status(request, response) {
+import database from "../../../../infra/database";
+
+async function Status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
   response.status(200).json("This will be the status endpoint at some point.");
 }
 
