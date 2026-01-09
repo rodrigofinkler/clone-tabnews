21db0913da443513e2adc10e3c0d88cdd9261bc1
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-09 13:54:12 -0300

    finalize the first implementation of the /status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,38 @@
 import database from "infra/database";
 
 async function Status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response.status(200).json("This will be the status endpoint at some point.");
+  const updatedAt = new Date();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult?.rows[0]?.server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult?.rows[0]?.max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query(
+    {
+      text: `SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    },
+    [databaseName],
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult?.rows[0]?.opened_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default Status;
