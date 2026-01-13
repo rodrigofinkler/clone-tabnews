c3b952aa6e436ce788a162bab7459f2cda3e3280
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-13 16:18:13 -0300

    fix `npm test` so that it can run on standalone mode by adding `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,11 @@
+import orchestrator from "tests/orchestrator.js";
+
 import database from "infra/database.js";
 
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
-
-beforeAll(cleanDatabase);
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
