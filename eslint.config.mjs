c9166ce2b051daf49ff1139aea6f654d614e6fed
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-13 23:37:09 -0300

    adds `Linting:ESLint` Github Action job

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+]);
