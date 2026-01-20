1400cd9bc4cf6128577ca9262749ec74f9364177
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-19 23:09:58 -0300

    feat: add `DatabaseInfo` to `/status` page

--- pages/status/index.js
+++ pages/status/index.js
@@ -12,6 +12,7 @@ export default function StatusPage() {
     <>
       <h1>Status</h1>
       <UpdatedAt />
+      <DatabaseInfo />
     </>
   );
 }
@@ -27,4 +28,33 @@ function UpdatedAt() {
   }
 
   return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function DatabaseInfo() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseVersion = "Carregando...";
+  let databaseMaxConnections = "Carregando...";
+  let databaseOpenedConnections = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseVersion = data.dependencies.database.version;
+    databaseMaxConnections = data.dependencies.database.max_connections;
+    databaseOpenedConnections = data.dependencies.database.opened_connections;
+  }
+
+  return (
+    <div style={{ marginTop: 16 }}>
+      Banco de Dados:
+      <p style={{ paddingLeft: 16, margin: 8 }}>Versão: {databaseVersion}</p>
+      <p style={{ paddingLeft: 16, margin: 8 }}>
+        Número máximo de conexões: {databaseMaxConnections}
+      </p>
+      <p style={{ paddingLeft: 16, margin: 8 }}>
+        Número de conexões abertas: {databaseOpenedConnections}
+      </p>
+    </div>
+  );
 }
