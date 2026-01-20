7fd57f264920a60898fdbbaf6e0140d6ada2be18
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-19 23:02:45 -0300

    feat: add initial `/status` page with updatedAt information

---
+++ pages/status/index.js
@@ -0,0 +1,30 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  console.log(responseBody);
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-br");
+  }
+
+  return <div>Última atualização: {updatedAtText}</div>;
+}
