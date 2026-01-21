3fd9d2fd560c815cbf7de200299cfbd285032e50
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-21 15:52:12 -0300

    refactor: add minor punctuation marks

--- infra/errors.js
+++ infra/errors.js
@@ -1,10 +1,10 @@
 export class InternalServerError extends Error {
   constructor({ cause }) {
-    super("Um erro interno não esperado aconteceu", {
+    super("Um erro interno não esperado aconteceu.", {
       cause,
     });
     this.name = "InternalServerError";
-    this.action = "Entre em contato com o suporte";
+    this.action = "Entre em contato com o suporte.";
     this.statusCode = 500;
   }
 
