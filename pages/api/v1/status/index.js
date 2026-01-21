3d697173543b386e29cb1df3e60eaa03632a0e9d
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-21 14:40:00 -0300

    feat: create and custom `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,36 +1,49 @@
 import database from "infra/database";
+import { InternalServerError } from "infra/errors";
 
 export default async function status(request, response) {
-  const updatedAt = new Date();
+  try {
+    const updatedAt = new Date();
 
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult?.rows[0]?.server_version;
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult?.rows[0]?.server_version;
 
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult?.rows[0]?.max_connections;
+    const databaseMaxConnectionsResult = await database.query(
+      "SHOW max_connections;",
+    );
+    const databaseMaxConnectionsValue =
+      databaseMaxConnectionsResult?.rows[0]?.max_connections;
 
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query(
-    {
-      text: `SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
-      values: [databaseName],
-    },
-    [databaseName],
-  );
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult?.rows[0]?.opened_connections;
-
-  return response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue,
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await database.query(
+      {
+        text: `SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+        values: [databaseName],
       },
-    },
-  });
+      [databaseName],
+    );
+    const databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult?.rows[0]?.opened_connections;
+
+    return response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionsValue),
+          opened_connections: databaseOpenedConnectionsValue,
+        },
+      },
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+    console.log(`\nErro dentro do catch do controller:`);
+    console.error(publicErrorObject);
+
+    console.error(publicErrorObject.toJSON());
+
+    response.status(500).json(publicErrorObject);
+  }
 }
