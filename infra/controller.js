63b85014ff0d585127c169350f6a8f482ba2d84c
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-21 21:39:19 -0300

    feat: add `infra/controller.js` to modularize the controllers' handlers

---
+++ infra/controller.js
@@ -0,0 +1,28 @@
+import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    statusCode: error.statusCode,
+    cause: error,
+  });
+
+  console.error(publicErrorObject);
+
+  console.error(publicErrorObject.toJSON());
+
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+const controller = {
+  errorHandlers: {
+    onError: onErrorHandler,
+    onNoMatch: onNoMatchHandler,
+  },
+};
+
+export default controller;
