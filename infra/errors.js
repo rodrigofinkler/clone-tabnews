198d21c694e24a5c05c7790131c2615e66b8b4a0
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-21 21:37:35 -0300

    feat: add `ServiceError` custom `Error` object

--- infra/errors.js
+++ infra/errors.js
@@ -36,3 +36,23 @@ export class MethodNotAllowedError extends Error {
     };
   }
 }
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
