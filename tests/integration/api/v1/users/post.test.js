ce4174a23d459fd31dba441ca8085b4cf8251172
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-23 16:34:23 -0300

    test: add test to create a new user using the newly created model and controller

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,45 @@
+import { version as uuidVersion } from "uuid";
+
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      const testUser = {
+        username: "michaelscott",
+        email: "michaelscott@dundermifflin.com",
+        password: "TobySux666",
+      };
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(testUser),
+      });
+
+      expect(response.status).toBe(201);
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: testUser.username,
+        email: testUser.email,
+        password: testUser.password,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+  });
+});
