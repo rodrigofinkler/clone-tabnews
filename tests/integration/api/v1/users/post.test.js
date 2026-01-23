1c6f7a22ae2a7b09a8a1288db236286931bc3d17
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-23 16:35:37 -0300

    test: add test to create a new user with a duplicated email

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -41,5 +41,45 @@ describe("POST /api/v1/users", () => {
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
+
+    test("With duplicated `email`", async () => {
+      const testUser = {
+        username: "emailduplicado1",
+        email: "duplicado@email.com",
+        password: "senha1234",
+      };
+
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(testUser),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...testUser,
+          username: "emailduplicado2",
+          email: testUser.email.toUpperCase(),
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "O email informado já está sendo utilizado",
+        action: "Utilize outro email para realizar o cadastro",
+        status_code: 400,
+      });
+    });
   });
 });
