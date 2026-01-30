c8c8c22ccab60310694e1b847accacaec0cf1d3d
Rodrigo Finkler <finklerrg@gmail.com>
2026-01-30 08:20:17 -0300

    feat: add support for `PATCH` method in `/api/v1/users/[username]`

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -6,11 +6,21 @@ import user from "models/user.js";
 const router = createRouter();
 
 router.get(getHandler);
-
-export default router.handler(controller.errorHandlers);
+router.patch(patchHandler);
 
 async function getHandler(request, response) {
   const { username } = request.query;
   const userFound = await user.findOneByUsername(username);
   return response.status(200).json(userFound);
 }
+
+async function patchHandler(request, response) {
+  const { username } = request.query;
+  const userInputValues = request.body;
+
+  const updatedUser = await user.update(username, userInputValues);
+
+  return response.status(200).json(updatedUser);
+}
+
+export default router.handler(controller.errorHandlers);
